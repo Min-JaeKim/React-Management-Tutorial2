ad7485d092ee6b10b084eb723788f58cb8110cab
ivo___ovi@naver.com <ivo___ovi@naver.com>
2020-05-31 19:56:04 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
-import Customer from './components/Customer';
-//import { render } from '@testing-library/react';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root:{
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
+
 
 const customers = [
   {
-  'id': '1',
+  'id': 1,
   'image': 'http://placeimg.com/64/64/1',
   'name': '홍길동',
   'birthday': '961222',
@@ -13,7 +31,7 @@ const customers = [
   'job': '대학생'
 },
 {
-  'id': '2',
+  'id': 2,
   'image': 'http://placeimg.com/64/64/2',
   'name': '홍시영',
   'birthday': '911222',
@@ -21,7 +39,7 @@ const customers = [
   'job': '래퍼'
 },
 {
-  'id': '3',
+  'id': 3,
   'image': 'http://placeimg.com/64/64/3',
   'name': '적길동',
   'birthday': '001222',
@@ -33,12 +51,24 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {
-          customers.map(c => {
-            return (
-              < Customer
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+<TableHead>
+<TableRow>
+<TableCell>번호</TableCell>
+<TableCell>이미지</TableCell>
+<TableCell>이름</TableCell>
+<TableCell>생년월일</TableCell>
+<TableCell>성별</TableCell>
+<TableCell>직업</TableCell>
+</TableRow>
+</TableHead>
+<TableBody>
+
+        {customers.map(c => { 
+          return ( <Customer
                 key={c.id}
                 id={c.id}
                 image={c.image}
@@ -50,9 +80,11 @@ class App extends Component {
             );
           })
         }
-       </div>
+        </TableBody>
+        </Table>
+       </Paper>
       );
     }
   }
 
-export default App;
+export default withStyles(styles)(App);
